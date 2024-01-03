20ab32e748780a1e6f458863b9b626d023fcab14
Shailesh Kadge <kadgeshailesh@gmail.com>
2024-01-03 16:09:41 +0530

    added fragment<></> and component Chai

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,9 +1,15 @@
 
+import Chai from './Chai'
 
 function App() {
 
   return (
+   <>
+   <Chai />
    <h1>Hey Shailesh, this is react with vite.</h1> 
+   
+   </>
+
   )
 }
 
---
+++ 01vitereact/src/Chai.jsx
@@ -0,0 +1,7 @@
+function Chai() {
+    return (
+        <h2> Mujhe chai chahiye</h2>
+    )
+}
+
+export default Chai
