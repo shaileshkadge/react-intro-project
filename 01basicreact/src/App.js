e8e954c7629804b46b0ea0c1d5b66ee39c38d099
Shailesh Kadge <kadgeshailesh@gmail.com>
2024-01-08 02:17:35 +0530

    Worked on tailwind CSS along with Vite react and used props

--- 01basicreact/src/App.js
+++ 01basicreact/src/App.js
@@ -1,10 +1,15 @@
 
 
 
+import Chai from './Chai';
+
 function App() {
   return (
+    <>
     <h1>Hey Shailesh!</h1>
+    <Chai />
+    </>
   );
 }
 
-export default App;
+export default App
---
+++ 01basicreact/src/Chai.jsx
@@ -0,0 +1,7 @@
+function Chai() {
+    return (
+        <h2> Here is your chai with vite. </h2>
+    )
+}
+
+export default Chai 
